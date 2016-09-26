a0c1d2f24899767fef98d04aa398f8225e08a1ed
DylanHul <dylan.hulain@gmail.com>
2016-09-26 13:08:49 +1300

    routes working added in github/linkedIn links

--- routes/ExLink.js
+++ routes/ExLink.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET contact listing. */
+/* GET ExLink listing. */
 router.get('/', function(req, res, next) {
-  res.render('link', { about: 'Links: '});
+  res.render('ExLink', { about: 'Links: '});
 });
 
 module.exports = router;
